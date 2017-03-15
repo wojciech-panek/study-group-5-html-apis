fc9b2cfa2651aa271b4d55c382d7dbf95eac80ba
Wojciech Panek <wpanek@apptension.com>
2017-03-15 14:38:14 +0100

    add userMedia audio streaming

--- app/src/components/master/audioLoader/audioLoader.component.js
+++ app/src/components/master/audioLoader/audioLoader.component.js
@@ -1,7 +1,7 @@
 import React, { Component, PropTypes } from 'react';
 
 
-const FREQUENCY = 10;
+const FREQUENCY = 50;
 
 export default class AudioLoader extends Component {
   static propTypes = {
@@ -13,12 +13,36 @@ export default class AudioLoader extends Component {
 
     this.audioContext = new AudioContext();
     this.audioAnalyser = this.audioContext.createAnalyser();
+    this.audioSource = null;
     this.frequencyData = null;
+    this.stream = null;
+
+    this.timeout = null;
   }
 
-  handleCanPlay = () => {
-    const source = this.audioContext.createMediaElementSource(this.audioNode);
-    source.connect(this.audioAnalyser);
+  handlePlay = () => {
+    this.audioSource = this.audioContext.createMediaElementSource(this.audioNode);
+    this.audioSource.connect(this.audioAnalyser);
+
+    this.start();
+  };
+
+  startRecording = () => {
+    if (!this.stream) {
+      navigator.getUserMedia({ audio: true }, (stream) => {
+        this.stream = stream;
+        this.audioSource = this.audioContext.createMediaStreamSource(this.stream);
+        this.audioSource.connect(this.audioAnalyser);
+
+        this.start();
+      }, () => {});
+    } else {
+      this.start();
+    }
+  };
+
+  start = () => {
+    this.stop();
 
     this.audioAnalyser.connect(this.audioContext.destination);
     this.audioAnalyser.fftSize = 64;
@@ -28,11 +52,18 @@ export default class AudioLoader extends Component {
     this.sendFrequencyData();
   };
 
+  stop = () => {
+    if (this.timeout) {
+      clearTimeout(this.timeout);
+      this.timeout = null;
+    }
+  };
+
   sendFrequencyData = () => {
     this.audioAnalyser.getByteFrequencyData(this.frequencyData);
 
     this.props.onAudioDataChange(this.frequencyData);
-    setTimeout(() => {
+    this.timeout = setTimeout(() => {
       this.sendFrequencyData();
     }, FREQUENCY);
   };
@@ -40,12 +71,20 @@ export default class AudioLoader extends Component {
   render() {
     return (
       <div className="audioLoader">
+        <h2>Audio:</h2>
         <audio
           controls
           ref={(ref) => (this.audioNode = ref)}
           src={require('../../../../data/song.mp3')}
-          onCanPlay={this.handleCanPlay}
+          onPlay={this.handlePlay}
+          onPause={this.stop}
         />
+
+        <div>
+          <h2>Microphone:</h2>
+          <button onClick={this.startRecording}>Start recording</button>
+          <button onClick={this.stop}>Stop recording</button>
+        </div>
       </div>
     );
   }
